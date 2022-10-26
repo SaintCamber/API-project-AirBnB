344779d57d7888ba84bf9fb705f27d8409dad8fe
Sarah <kretaksson@gmail.com>
2022-10-25 23:40:27 -0700

    avgRating added now just need to add a default scope i think and it will be ready

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,16 +1,24 @@
 const express = require('express');
-
+const sequelize = require('sequelize')
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { Spot } = require('../../db/models');
+const { Spot ,Review} = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 // ...
 const router = express.Router();
 
 
-router.get('/',async (req,res)=>{
-    let allSpots = await Spot.findAll({})
-    res.json(allSpots)
+router.get('/',requireAuth, async (req,res)=>{
+    let allSpots = await Spot.findAll({
+        include:{model:Review},
+        attributes: {
+          include: [
+            [sequelize.fn('AVG', sequelize.col('stars')), 'AvgRating']
+          ]
+        }
+      });
+
+      res.json(allSpots)
 })
 
 module.exports = router;
