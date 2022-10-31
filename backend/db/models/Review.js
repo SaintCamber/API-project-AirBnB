510271820ea27d0e36788c075b2f3415c33a486d
Sarah <kretaksson@gmail.com>
2022-10-30 19:12:43 -0700

    ffff

--- backend/db/models/Review.js
+++ backend/db/models/Review.js
@@ -20,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Review.init({
-    spotId: DataTypes.INTEGER,
+    SpotId: DataTypes.INTEGER,
     userId: DataTypes.INTEGER,
     review: DataTypes.STRING,
     stars: DataTypes.INTEGER
